aad23d693ea096edd2a86536fd0560a86958179b
shaoguangleo <shaoguangleo@gmail.com>
2015-09-19 13:37:34 +0800

    Finish load the UV data

---
+++ load_uvsel_uv.py
@@ -0,0 +1,20 @@
+def load_uvsel_uv(uv_filename):
+    # Pick up the data info
+    # uv data parameters:
+    # uu , vv, amp, phs, wt, uvscale, cif, isub, base, ut
+    # 1  ,  2,   3,   4,  5,       6,   7,    8,    9, 10
+    print 'Now loading uv file...'
+
+    fid = open(uv_filename)
+    content = fid.readlines()
+
+    uv_data = []
+    #for i in range(len(content)):
+    for i in content:
+        # This is the data info
+        # uv_data.append(content(i).split('=')[1])
+        uv_data.append(i.split('=')[1])
+        # This is the describe info
+        # uv_data.append(content(i).split('=')[0])
+
+    return uv_data
