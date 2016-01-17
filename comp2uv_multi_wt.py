120bc72ec4b2540dc9c688cbf467cdf4a719a912
shaoguangleo <shaoguangleo@gmail.com>
2016-01-17 18:59:01 +0800

    Add comp2uv_multi_wt

---
+++ comp2uv_multi_wt.py
@@ -0,0 +1,13 @@
+import comp2uv_multi
+import math
+
+def comp2uv_multi_wt(x_fit_multi,uvData):
+    uv_re_im_fit_multi = comp2uv_multi.comp2uv_multi(x_fit_multi,uvData)
+    uv_re_im_fit_multi_wt = uv_re_im_fit_multi
+    weight_idx = 5
+
+    uv_re_im_fit_multi_wt
+
+    for i in range(len(uv_re_im_fit_multi_wt)):
+        uv_re_im_fit_multi_wt[i][0] = uv_re_im_fit_multi[i][0] * math.sqrt(uvData[i][weight_idx-1])
+        uv_re_im_fit_multi_wt[i][1] = uv_re_im_fit_multi[i][1] * math.sqrt(uvData[i][weight_idx-1])
