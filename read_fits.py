e2c0928e1df9b1f044729fc67e51f54faf73effe
shaoguangleo <shaoguangleo@gmail.com>
2016-01-17 21:38:19 +0800

    Add reading fits file

---
+++ read_fits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+"""
+This script will read the fits result file
+@version:0.9
+@contact: sgguo@shao.ac.cn
+@author:{Guo Shaoguang<mailto:sgguo@shao.ac.cn>}
+"""
+
+import numpy as np
+
+﻿def read_fits(fits_file):
+    print 'ready read the fits file'
+    a = open(fits_file)
+    rst = []
+    for i in a:
+        rst.append([float(i.split('\t')[j]) for j in range(1024)])
+    return rst
