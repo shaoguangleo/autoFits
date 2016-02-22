dd44b72dfece7ad257dd185b0da7d128cc97f25b
shaoguangleo <shaoguangleo@gmail.com>
2016-02-22 10:20:16 +0800

    Add return row and column

---
+++ cal_col_row.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+This script will comp the uv multi data
+@version:0.9
+@contact: sgguo@shao.ac.cn
+@author:{Guo Shaoguang<mailto:sgguo@shao.ac.cn>}
+"""
+import numpy as np
+
+def cal_col_row(data_list):
+    total_size = np.size(data_list)
+    col = np.size(data_list[0])
+    row = total_size / col
+    return row,col
