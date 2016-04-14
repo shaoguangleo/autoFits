67f393e6ad69a83b773f3bb5746b9f747d438ec3
shaoguangleo <shaoguangleo@gmail.com>
2016-04-14 16:43:30 +0800

    Finish plot fits file

--- plot_fits.py
+++ plot_fits.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 This script will plot fits file
-@version:0.9
+@version:1.0
 @contact: sgguo@shao.ac.cn
 @author:{Guo Shaoguang<mailto:sgguo@shao.ac.cn>}
 """
@@ -11,9 +11,10 @@ import matplotlib.pyplot as plt
 
 
 def plot_fits(my_fits,fidx):
+    print '> Plotting fits file'
     First_cntr_ratio = 1024
-    #my_fits_max = max(my_fit)
-    my_fits_max = 100.0
+    my_fits_max = my_fits.max()
+    #my_fits_max = 100.0
     First_cntr = my_fits_max/First_cntr_ratio
 
     contour_min = np.log2(First_cntr)
@@ -26,7 +27,8 @@ def plot_fits(my_fits,fidx):
     plt.hold(True)
     #plt.contour(my_fits,v)
     plt.contour(my_fits,112)
-    plt.show()
     plt.axis('equal')
     plt.grid
     plt.title('Clean Map')
+    plt.savefig('clean_map.png')
+    #plt.show()
