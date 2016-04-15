82e71ef64f5890bf5f5f3e59e5d8fd2058ce1bbb
shaoguangleo <shaoguangleo@gmail.com>
2016-04-15 16:12:30 +0800

    Finished maplot

--- maplot.py
+++ maplot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 This script will plot uv data
-@version:0.99
+@version:1.0
 @contact: sgguo@shao.ac.cn
 @author:{Guo Shaoguang<mailto:sgguo@shao.ac.cn>}
 """
@@ -19,6 +19,18 @@ def maplot(uvData, my_units, domap):
     # Plot ,dirty beam,residual map,restored map
     # Prepare uv bin
     [uvbin,uvbin_array,uvb] =uv_bin.uv_bin(uvData,my_units)
+    #if all_class.debug:
+    if not all_class.debug:
+        print uvb.nu
+        print uvb.nv
+    plt.figure()
+    plt.plot(uvbin)
+    plt.title('UVBIN')
+    plt.savefig('uvbin.png')
+
+    plt.figure()
+    plt.contour(uvbin_array)
+    plt.savefig('uvbin_arr.png')
 
     # for debug
     trace =0
@@ -49,10 +61,6 @@ def maplot(uvData, my_units, domap):
     gcf.rxft = rxft
     gcf.ryft = ryft
 
-    if all_class.debug:
-        print 'uvbin is '
-        print uvbin
-
     [cntr_ptr_vector,cntr_ptr_vector_array] = uv_grid.uv_grid(uvData, my_units, uvbin, uvb, gcf, domap)
     if all_class.debug:
         print '-'*80
@@ -62,19 +70,22 @@ def maplot(uvData, my_units, domap):
         temp_rst.write(str(cntr_ptr_vector))
         temp_rst.close()
         print '-'*80
-        import time
-        time.sleep(5)
 
     fidx = 10
     if all_class.debug:
         trace = 1
-    #TODO Here should check the value cntr_ptr_vector_array, seems different
     if trace ==1:
         fidx=fidx+1
         plt.figure(fidx)
         plt.imshow(abs(cntr_ptr_vector_array))
         plt.title('maplot(), uvdata')
-        plt.show()
+        #plt.savefig('maplot_uv.png')
+
+    fidx=fidx+1
+    plt.figure(fidx)
+    plt.imshow(abs(cntr_ptr_vector_array))
+    plt.title('maplot(), uvdata')
+    plt.savefig('maplot.png')
 
 
     #ifft
@@ -84,17 +95,19 @@ def maplot(uvData, my_units, domap):
     #print 'The max'
     #print cntr_ptr_vector_array.max()
 
-    #TODO the image is different with Matlab
-    #if trace ==0:
-    if trace ==1:
+    if trace ==0:
+    #if trace ==1:
         fidx=fidx+1
         plt.figure(fidx)
         #plt.imshow(uvbin_array_conj_shift2_ifft2./max(uvbin_array_conj_shift2_ifft2(:)))
         #plt.imshow(uvbin_array_conj_shift2_ifft2)
-        plt.imshow(uvbin_array_conj_shift2_ifft2/uvbin_array_conj_shift2_ifft2.max())
-        #plt.imshow(uvbin_array_conj_shift2_ifft2/(3819.1 + 0.51204j))
+        print 'max'
+        print uvbin_array_conj_shift2_ifft2.max()
+        print type(uvbin_array_conj_shift2_ifft2)
+        #plt.imshow(uvbin_array_conj_shift2_ifft2/uvbin_array_conj_shift2_ifft2.max())
+        plt.contour(uvbin_array_conj_shift2_ifft2)
         plt.title('uvdata ifft')
-        #plt.show()
+        plt.savefig('uv_data_ifft.png')
 
 #  * Multiply the image throughout by the sensitivity function to remove the
 #%  * gridding convolution function.
@@ -116,13 +129,18 @@ def maplot(uvData, my_units, domap):
             y_xu = np.ceil(round(y*scale_y))
             uvbin_array_conj_shift2_ifft2_rft[y][x] = ryft[int(y_xu)]*rxft[int(x_xu)]*uvbin_array_conj_shift2_ifft2[y][x]
 
-    if trace ==1:
-        fidx=fidx+1
-        plt.figure(fidx)
+    plt.figure()
+    plt.contour(uvbin_array_conj_shift2_ifft2_rft)
+    plt.title('image_remove_gridding')
+    plt.savefig('remove_gridding.png')
+
+    if trace ==0:
+        #fidx=fidx+1
+        plt.figure()
         #plt.imshow(uvbin_array_conj_shift2_ifft2_rft./max(uvbin_array_conj_shift2_ifft2_rft(:)));
-        plt.imshow(uvbin_array_conj_shift2_ifft2_rft)
-        plt.title('maplot(), map image')
-        plt.show()
+        #plt.imshow(uvbin_array_conj_shift2_ifft2_rft)
+        #plt.title('maplot(), map image')
+        #plt.savefig('maplot_1.png')
     # Make sure the image pixel is not complex, if it is complex means program have error
     # uvbin_array_conj_shift2_ifft2_rft_abs = abs(uvbin_array_conj_shift2_ifft2_rft);
     # print np.array(uvbin_array_conj_shift2_ifft2_rft)
